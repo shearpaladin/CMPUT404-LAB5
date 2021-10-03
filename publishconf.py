6365913a7765bfa9d650600951b4bf415f05234a
shearpaladin <sandy6@ualberta.ca>
2021-10-02 21:12:31 -0600

    github page pls work

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://shearpaladin.github.io/CMPUT404-LAB5'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
